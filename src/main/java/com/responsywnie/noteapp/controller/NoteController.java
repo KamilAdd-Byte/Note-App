a810a0f2e5cb5b298474e0c9562692b03fe3dd6b
KamilAdd-Byte <kamillodzinski09@gmail.com>
2021-02-25 17:52:08 +0100

    List all note mapping on two controller. View / and /index

--- src/main/java/com/responsywnie/noteapp/controller/NoteController.java
+++ src/main/java/com/responsywnie/noteapp/controller/NoteController.java
@@ -6,7 +6,9 @@ import com.responsywnie.noteapp.service.NoteServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class NoteController {
@@ -17,28 +19,34 @@ public class NoteController {
     @Autowired
     private final NoteServiceImpl service;
 
-    public NoteController(NoteDAO repository, NoteServiceImpl service) {
+    public NoteController(final NoteDAO repository, final NoteServiceImpl service) {
         this.repository = repository;
         this.service = service;
     }
 
-    @GetMapping("/all")
-    public String showNote(Model model) {
-        model.addAttribute("listNote",repository.findAll());
+    @GetMapping("/index")
+    public String showFormAllNote(Model model) {
+        model.addAttribute("listNote", repository.findAll());
+        return "index";
+    }
+
+    @GetMapping("/")
+    public String showAllNote(Model model) {
+        model.addAttribute("listNote", repository.findAll());
         return "index";
     }
 
     @GetMapping("/showNewNoteForm")
-    public String showNewNoteForm(Model model){
+    public String showNewNoteForm(Model model) {
         Note note = new Note();
-        model.addAttribute("note",note);
+        model.addAttribute("note", note);
         return "new_note";
     }
 
     @PostMapping(value = "/new_note")
-    public String addNote(@ModelAttribute("note") Note note,Model model) {
+    public String addNewNote(@ModelAttribute("note") Note note, Model model) {
         service.addNote(note);
-        model.addAttribute("message","Dodano notatkę!");
-        return "index";
+        model.addAttribute("message", "Dodano notatkę!");
+        return "redirect:/index";
     }
 }
