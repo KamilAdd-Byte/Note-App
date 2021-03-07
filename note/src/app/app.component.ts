eb7208f4dcdcc2466841cf7913b1fb40cb31471c
KamilAdd-Byte <kamillodzinski09@gmail.com>
2021-03-07 11:56:56 +0100

    Add method onSwitchForm on navi

--- note/src/app/app.component.ts
+++ note/src/app/app.component.ts
@@ -7,5 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'note-app';
+  showForm = false;
+
+  onSwitchForm(): void {
+    this.showForm = !this.showForm;
+  }
 
 }
