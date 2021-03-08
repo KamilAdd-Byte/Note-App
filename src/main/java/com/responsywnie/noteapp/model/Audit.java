d78659126badade8426a4530e67147f7eb781e9d
KamilAdd-Byte <kamillodzinski09@gmail.com>
2021-03-08 21:46:23 +0100

    Work date Note. Still dosen't work's

--- src/main/java/com/responsywnie/noteapp/model/Audit.java
+++ src/main/java/com/responsywnie/noteapp/model/Audit.java
@@ -1,22 +1,26 @@
 package com.responsywnie.noteapp.model;
 
 import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Embeddable;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 import java.time.LocalDateTime;
+import java.util.Date;
 
 @Embeddable
 @Getter
+@Setter
 public class Audit {
-    private LocalDateTime createOn;
-    private LocalDateTime updateOn;
+    private Date createOn;
+    private Date updateOn;
     @PrePersist
     public void prePersist(){
-        createOn = LocalDateTime.now();
+        createOn = new Date();
     }
     @PreUpdate
     public void preUpdate(){
-        updateOn = LocalDateTime.now();
+        updateOn = new Date();
     }
 }
