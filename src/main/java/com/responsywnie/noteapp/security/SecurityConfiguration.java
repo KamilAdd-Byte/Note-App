930a2c7ebf943b2f3af3167dc32d6f5d9bd00b8e
KamilAdd-Byte <kamillodzinski09@gmail.com>
2021-03-08 20:51:15 +0100

    Add Security in pom.xml and configure auth

--- src/main/java/com/responsywnie/noteapp/security/SecurityConfiguration.java
+++ src/main/java/com/responsywnie/noteapp/security/SecurityConfiguration.java
@@ -1,45 +1,32 @@
 package com.responsywnie.noteapp.security;
 
-import lombok.var;
-import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
-import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
-import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
-import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
-import org.springframework.security.core.session.SessionRegistryImpl;
-import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
-import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-@KeycloakConfiguration
-public class SecurityConfiguration extends KeycloakWebSecurityConfigurerAdapter {
-    @Override
-    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
-        return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
-    }
+@Configuration
+@EnableGlobalMethodSecurity(securedEnabled = true)
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Autowired
-    void configureGlobal(AuthenticationManagerBuilder auth){
-        var authorityMapper = new SimpleAuthorityMapper();
-        authorityMapper.setPrefix("ROLE_");
-        authorityMapper.setConvertToUpperCase(true);
-        KeycloakAuthenticationProvider keycloakAuthenticationProvider = keycloakAuthenticationProvider();
-        keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(authorityMapper);
-        auth.authenticationProvider(keycloakAuthenticationProvider);
+    public void configureAuth(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("Kasia").password("{noop}mysza300").roles("USER","ADMIN")
+                .and()
+                .withUser("Kamil").password("{noop}mysza300").roles("USER","ADMIN");
     }
     @Override
-    protected void configure(final HttpSecurity http) throws Exception {
-        super.configure(http);
+    protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/")
-                .hasRole("USER")
-                .anyRequest()
-                .permitAll();
-    }
-    @Bean
-    KeycloakSpringBootConfigResolver keycloakSpringBootConfigResolver(){
-        return new KeycloakSpringBootConfigResolver();
+               .antMatchers("/","/showNewNoteForm","/about","/assets/**").permitAll()
+                .antMatchers("/**","/showNewNoteForm").hasRole("ADMIN")
+                .antMatchers("/**","/showNewNoteForm").hasRole("USER")
+                .anyRequest().authenticated()
+                .and().formLogin().defaultSuccessUrl("/")
+                .and().logout().logoutSuccessUrl("/");
     }
 }
