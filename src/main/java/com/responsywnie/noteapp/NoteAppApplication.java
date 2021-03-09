e5c781fdf4fea8c2bf9d802564b009d692be0042
KamilAdd-Byte <kamillodzinski09@gmail.com>
2021-03-09 14:40:21 +0100

    Add link img N on web

--- src/main/java/com/responsywnie/noteapp/NoteAppApplication.java
+++ src/main/java/com/responsywnie/noteapp/NoteAppApplication.java
@@ -1,11 +1,19 @@
 package com.responsywnie.noteapp;
 
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.SearchStrategy;
+import org.springframework.boot.autoconfigure.web.servlet.error.BasicErrorController;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorViewResolver;
+import org.springframework.boot.web.servlet.error.ErrorAttributes;
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.context.annotation.Bean;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 import javax.validation.Validator;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 public class NoteAppApplication {
